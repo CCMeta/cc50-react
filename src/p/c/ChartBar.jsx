2113bd9b094c1bdcf547b704eb9e911bd08283c4
ccmeta <ccmeta@icloud.com>
2022-08-13 09:44:47 +0800

    to be dynamic

--- src/p/c/ChartBar.jsx
+++ src/p/c/ChartBar.jsx
@@ -3,34 +3,25 @@ import { ResponsiveBar } from '@nivo/bar'
 
 const _data = [
   {
-    "date": "MON",
-    "DL": 92,
-    "UL": 92,
+    "date": "MON", "DL": 92, "UL": 92,
   },
   {
-    "date": "TUR",
-    "UL": 200,
-    "DL": 96,
+    "date": "TUR", "UL": 200, "DL": 96,
   },
   {
-    "date": "THR", "DL": 43,
-    "UL": 184,
+    "date": "THR", "DL": 43, "UL": 184,
   },
   {
-    "date": "WES", "DL": 84,
-    "UL": 50,
+    "date": "WES", "DL": 84, "UL": 50,
   },
   {
-    "date": "FRI", "DL": 91,
-    "UL": 133,
+    "date": "FRI", "DL": 91, "UL": 133,
   },
   {
-    "date": "SAT", "DL": 150,
-    "UL": 50,
+    "date": "SAT", "DL": 150, "UL": 50,
   },
   {
-    "date": "SUN", "DL": 139,
-    "UL": 99,
+    "date": "SUN", "DL": 139, "UL": 99,
   }
 ]
 
@@ -41,6 +32,9 @@ export const MyResponsiveBar = ({ data }) => (
       'UL',
       'DL'
     ]}
+    axisLeft={{
+      tickValues: [0, 50, 100, 150, 200, 250]
+    }}
     layers={['grid', 'axes', 'bars', '', '']}
     borderRadius={2}
     indexBy="date"
