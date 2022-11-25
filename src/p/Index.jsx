8e35700f2c547788af87217b2eb05a81d6d496be
CCMeta <ccmeta@icloud.com>
2022-11-25 13:10:02 +0800

    wifi.stat.5g.get && wifi.stat.24g.get

--- src/p/Index.jsx
+++ src/p/Index.jsx
@@ -208,10 +208,13 @@ export default () => {
     data_iwinfo_5G.set(await webcmd(`wifi.status.5g.get`))
     // data_iwinfo_24G.set(await fetching_iwinfo_24G())
     data_iwinfo_24G.set(await webcmd(`wifi.status.24g.get`))
+    
     // thw wifi devices of wifi info per devices , such as PhyMode HE=AX VHT=AC
     // https://192.168.1.1/cgi-bin/luci/admin/mtk/wifi/sta_info/rai0/MT7915D.1.2?1659322511882
-    data_clients_info_5G.set(await fetching(null, 'wifi', `/sta_info/rai0`))
-    data_clients_info_24G.set(await fetching(null, 'wifi', `/sta_info/ra0`))
+    // data_clients_info_5G.set(await fetching(null, 'wifi', `/sta_info/rai0`))
+    data_clients_info_5G.set(await webcmd(`wifi.stat.5g.get`))
+    // data_clients_info_24G.set(await fetching(null, 'wifi', `/sta_info/ra0`))
+    data_clients_info_24G.set(await webcmd(`wifi.stat.24g.get`))
 
     data_wan_network_interface_dump.set(
       (await $rpc.post("network.interface", "dump"))?.[1]?.interface.find(i => i.interface === `wan`)
