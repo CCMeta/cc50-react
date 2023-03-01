3b24cf35d85b8a67f94c7c297e6b295e4fe989b7
CCMeta <ccmeta@icloud.com>
2023-03-01 13:00:10 +0800

    hellapi

---
+++ openwrt/hellapi_init.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+// cause MTK modem policy, when modem is down the ccmni interface order number may change as random
+// e.g: ccmni1 => ccmni11
+int seekCurrentModemInterface()
+{
+  char *currentModem;
+
+  // ubus call network.interface.wan dump
+  // or
+  // ifconfig  ifconfig | grep ccmni
+  currentModem = "$currentModem";
+
+  "uci set hellapi.set.currentModem = $currentModem";
+  "vnstat --create -i $currentModem && vnstat -u";
+
+  // So, when sb call hellapi he need to find current modem interface from uci.
+  // like vnstat, cat /sys/class/net/ccmni/blabla
+  return 0;
+}
+
+// To fix wifi initial quectel fuck MTK policy issue.
+int wifiReload()
+{
+  system("wifi reload");
+  return 0;
+}
+
+int main()
+{
+  if (seekCurrentModemInterface() != 0)
+  {
+    printf("seekCurrentModemInterface fucked!");
+    return -1;
+  }
+  if (wifiReload() != 0)
+  {
+    printf("wifiReload fucked!");
+    return -1;
+  }
+
+  printf("hellapi_init run finished!");
+  return 0;
+}
---
+++ openwrt/hellapid.c
@@ -0,0 +1,43 @@
+
+int showSpeed()
+{
+  static struct hwnat_mib_all_ip_mac_args_total argsSpeed; // define clients speed array
+  if (ql_pop_lan_devices_packets_total(&argsSpeed) == 0)
+  {
+    // argsSpeed is changed
+
+    // argsSpeed->entries[i].packets_total.mac_addr
+    /*
+          argsSpeed->entries[i].packets_currect.packets.tx_bytes,
+          argsSpeed->entries[i].packets_currect.packets.rx_bytes,
+          argsSpeed->entries[i].packets_total.packets.tx_bytes,
+          argsSpeed->entries[i].packets_total.packets.rx_bytes
+    */
+
+    uci set hellapi.speed.data = argsSpeed // save new value to uci 
+    uci set hellapi.speed.timestamp = now // save now timestamp value to uci 
+    uci commit //
+  }
+  else
+  {
+    // probably return -1  , it means no data changed
+    // do nothing
+  }
+
+  // ql_pop_lan_devices_packets_total function is fucked  what i should do next? restart the process???
+  return 0;
+}
+
+int main()
+{
+  int interval  = 1;
+  // this is daemon process of hellapi 
+  while (1)
+  {
+    sleep(interval); // while loop duration
+
+    //counter of speed_monitor 
+    showSpeed();
+  }
+  return 0;
+}
